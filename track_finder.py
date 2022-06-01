67e2a8c17a4e77be09bc68652f4c0f2450e0d419
Hugo De Santiago <hugodesantiago@utexas.edu>
2022-05-31 23:15:31 -0500

    Now track_finder.py chooses a track by itself

--- track_finder.py
+++ track_finder.py
@@ -1,15 +1,27 @@
 import spotipy
 import constants
+import random
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Move to another place later
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-  client_id=constants.spotify_client_id,
-	client_secret=constants.spotify_client_secret))
 
-house_playlist = sp.playlist_tracks(playlist_id="5ZhYpkuIMDnaX8uDWdBbAv")
 
-for track in house_playlist["items"]:
+def main():
+  sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=constants.spotify_client_id,
+    client_secret=constants.spotify_client_secret))
+
+  house_playlist = sp.playlist_tracks(playlist_id="5ZhYpkuIMDnaX8uDWdBbAv")
+  playlist_len = find_playlist_length('5ZhYpkuIMDnaX8uDWdBbAv')
+  track_number = random.randrange(1,playlist_len)
+
+  # Find the actual song that pertains to the randonmly generated song_number
+  # You have to loop through the iterator since Spotify only displays 100 songs
+  # at a time, if you want the rest of the songs you have to scroll
+  # See the find_playlist_length function for the same logic
+  for i in range(int(track_number/100)):
+    house_playlist = sp.next(house_playlist)
+
+  track = house_playlist["items"][track_number%100-1]
   #URI
   track_uri = track["track"]["uri"]
   
@@ -32,9 +44,40 @@ for track in house_playlist["items"]:
   track_pop = track["track"]["popularity"]
 
   #Track Features
-  track_danceability = sp.audio_features(track_uri)[0]['danceability']
-  track_bpm = sp.audio_features(track_uri)[0]['tempo']
+  track_danceability = str(round(sp.audio_features(track_uri)[0]['danceability'], 2) * 100) + "%"
+  track_bpm = int(round(sp.audio_features(track_uri)[0]['tempo'], 0))
+  track_energy = sp.audio_features(track_uri)[0]['energy']
   
-  print(track_name, str(track_danceability*100)+"%", track_bpm)
+  print("Track:", track_name)
+  print("Artist:", artist_name)
+  print("BPM:", track_bpm)
+  print("Danceability:", track_danceability)
+  print("Energy:", track_energy)
+
+
+
+def find_playlist_length(playlist_id):
+  sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=constants.spotify_client_id,
+    client_secret=constants.spotify_client_secret))
+
+  counter = 0
+  playlist = sp.playlist_tracks(playlist_id)
+
+  # Spotify API limits house_playlist to 100 tracks, so I need the following piece
+  # of code to continue looping through the songs.
+  while playlist:
+    for track in playlist["items"]:
+      counter += 1
+    if playlist['next']:
+      playlist = sp.next(playlist)
+    else:
+      playlist = None
+  return counter
+
+main()
+  
   
 
+
+  
